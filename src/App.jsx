d911462ca7f1213b1a0329fe17ae9efd22c04266
omkhodse18 <omkarkhodse.18@gmail.com>
2024-03-19 23:25:54 +0530

    Done with styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import Tag from "./components/Tag";
 
 export default function App() {
   return (
-    <div>
-      <h1>
-        RANDOM GIF
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+      <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
+       px-10 py-2 text-4xl font-bold ">
+        RANDOM GIFS
       </h1>
-
-      <div >
-        <Random/>
-        <Tag/>
+      
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random />
+        <Tag />
       </div>
 
     </div>
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,8 +1,6 @@
 import Spinner from "./Spinner";
 import useGif from "../hooks/useGif";
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY; 
-
 export default function Random() {
 
   const {gif, loading, fetchData} = useGif();
