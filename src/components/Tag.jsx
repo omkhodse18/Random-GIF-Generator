1fb4f300c8a6431e2ca234eb37b91b94c97b2b66
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-11 14:20:41 +0530

    Done

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,36 +1,39 @@
-import { useState } from "react"
-import useGif from "../hooks/useGif";
-import Spinner from "./Spinner";
+import React, { useState } from 'react'
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
-export default function Tag() {
-  
-  const [tag, setTag] = useState('programming');
-  const {gif, loading, fetchData} = useGif();
+const Tag = () => {
+  const [tag, setTag] = useState('car');
+
+  const {gif, loading, fetchData} = useGif(tag);
+
 
   return (
-    <div>
-      <div className='w-1/2 bg-blue-500 rounded-lg border border-black
-      flex flex-col items-center gap-y-5 mt-[15px]'>
+    <div className='w-1/2  bg-blue-500 rounded-lg border border-black
+    flex flex-col items-center gap-y-5 mt-[15px]'>
 
-      <h1 className='mt-[15px] text-2xl underline uppercase font-bold'>Random {tag} GIF</h1>
+      <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> Random {tag} Gif</h1>
 
       {
-        loading ? <Spinner/> : <img src={gif} width="450"/>
+        loading ? (<Spinner/>) : (<img src= {gif} alt='tag-gif' width="450" />)
       }
 
       <input 
-      className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center'
-      onChange={(event) =>  setTag(event.target.value)}
-      value={tag}
+        className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center'
+        onChange={(event) =>  setTag(event.target.value)}
+        value={tag}
       />
+      
 
+      <button onClick={() => fetchData(tag)}
+      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
 
-      <button onClick={()=> fetchData(tag)}
-        className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
-        Generate
+       Generate
+
       </button>
 
     </div>
-    </div>
-  );
+  )
 }
+
+export default Tag
